b195c2ffb3adf5aea8ff5a29c78a4fb8e71f8959
AlexZd <alexzd89@mail.ru>
2016-06-08 12:03:32 +0400

    Defined URL transformer

--- ApplicationSupport/Classes/Transformer.swift
+++ ApplicationSupport/Classes/Transformer.swift
@@ -10,6 +10,7 @@ public typealias ForwardBlock = ((AnyType) -> Any?)
 public typealias BacwardBlock = ((Any) -> AnyType?)
 
 public struct Transformer {
+    
     public var forward: ForwardBlock?
     public var backward: BacwardBlock?
     
@@ -17,7 +18,22 @@ public struct Transformer {
         self.forward = forward
         self.backward = backward
     }
+    
     public init(backward: BacwardBlock?) {
         self.backward = backward
+    }
+    
+    public struct Defined {
+        public static let URLTransformer = Transformer(forward: { (value) -> Any? in
+            if let url = value as? String {
+                return URL(string: url)
+            }
+            return nil
+            }, backward: { (value) -> AnyType? in
+                if let url = value as? URL {
+                    return url.absoluteString
+                }
+                return nil
+        })
     }
 }
