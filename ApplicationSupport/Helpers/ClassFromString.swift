69c5644d13c95ee342a35f55594227e4d8a07c14
Alex <Alex@MacBook-Pro-Alex.local>
2016-07-11 21:19:27 +0400

    ClassFromString fix

--- ApplicationSupport/Helpers/ClassFromString.swift
+++ ApplicationSupport/Helpers/ClassFromString.swift
@@ -11,7 +11,7 @@ import Foundation
 public func ClassFromString(className: String) -> AnyClass {
     var cls : AnyClass? = NSClassFromString(className)
     if let appName = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleExecutable") as? String where cls == nil {
-        let name = appName + className
+        let name = appName + "." + className
         cls = NSClassFromString(name)
         if cls == nil {
             fatalError("Unable to find `\(name)`")
