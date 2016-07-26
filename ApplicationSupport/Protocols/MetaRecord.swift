49de2c13cd353ccc2aefbead1dd8fc7f1bbcff54
AlexZd <alexzd89@mail.ru>
2016-07-26 09:17:32 +0400

    MetaRecord

---
+++ ApplicationSupport/Protocols/MetaRecord.swift
@@ -0,0 +1,22 @@
+//
+//  MetaRecord.swift
+//  Pods
+//
+//  Created by Alex Zdorovets on 7/26/16.
+//
+//
+
+import Foundation
+
+public protocol MetaRecord {}
+
+public extension MetaRecord {
+    public final static var modelName: String {
+        let reflect = _reflect(self)
+        var className = "\(reflect.summary)"
+        if let typeRange = className.rangeOfString(".Type") {
+            className.replaceRange(typeRange, with: "")
+        }
+        return className.lowercaseString
+    }
+}
