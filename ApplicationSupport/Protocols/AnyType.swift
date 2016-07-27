ddd4f664393351456e8f3c9faf860d355a56e9b1
Vlad Gorbenko <mojidabckuu.22.06.92@gmail.com>
2016-07-27 14:26:16 +0400

    Hide AnyType

--- ApplicationSupport/Protocols/AnyType.swift
+++ ApplicationSupport/Protocols/AnyType.swift
@@ -7,33 +7,33 @@
 //
 
 // TODO: Find a way make it as Hashable
-public protocol AnyType {
-    var rawType: String { get }
-}
+// public protocol AnyType {
+//     var rawType: String { get }
+// }
 
-extension AnyType {
-    public var rawType: String {
-        return "\(self)"
-    }
-}
+// extension AnyType {
+//     public var rawType: String {
+//         return "\(self)"
+//     }
+// }
 
-public func == (lhs: AnyType?, rhs: AnyType?) -> Bool {
-    if let left = lhs {
-        if let right = rhs {
-            if left.rawType != right.rawType { return false }
-            switch left.rawType {
-            case "Bool": return (left as! Bool) == (right as! Bool)
-            case "Date": return (left as! Date).compare(right as! Date) == NSComparisonResult.OrderedSame
-            case "Double": return (left as! Double) == (right as! Double)
-            case "Float": return (left as! Float) == (right as! Float)
-            case "Int": return (left as! Int) == (right as! Int)
-            case "String": return (left as! String) == (right as! String)
-            default: return false
-            }
-        }
-        return false
-    } else if let _ = rhs {
-        return false
-    }
-    return true
-}
+// public func == (lhs: AnyType?, rhs: AnyType?) -> Bool {
+//     if let left = lhs {
+//         if let right = rhs {
+//             if left.rawType != right.rawType { return false }
+//             switch left.rawType {
+//             case "Bool": return (left as! Bool) == (right as! Bool)
+//             case "Date": return (left as! Date).compare(right as! Date) == NSComparisonResult.OrderedSame
+//             case "Double": return (left as! Double) == (right as! Double)
+//             case "Float": return (left as! Float) == (right as! Float)
+//             case "Int": return (left as! Int) == (right as! Int)
+//             case "String": return (left as! String) == (right as! String)
+//             default: return false
+//             }
+//         }
+//         return false
+//     } else if let _ = rhs {
+//         return false
+//     }
+//     return true
+// }
