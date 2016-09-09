fe3c1cee9d1de0b9038a07a8f300fc907d9ebe08
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2016-09-09 16:51:24 +0400

    Add extra utils

--- ApplicationSupport/Extensions/String+Extension.swift
+++ ApplicationSupport/Extensions/String+Extension.swift
@@ -9,11 +9,10 @@
 // extension String: AnyType, DatabaseRepresentable {
 //     //MARK: - AnyType
 //     public var rawType: String { return "String" }
-    
+
 //     //MARK: - DatabaseRepresentable
 //     public var dbValue: AnyType { return "'\(self)'" }
 // }
-
 
 public extension String {
     var untrim: String {
@@ -33,5 +32,40 @@ public extension String {
     
     public var embrace: String {
         return "(" + self + ")"
+    }
+}
+
+public extension String {
+    var camelString: String {
+        return self.characters.split("_").map({ return String($0).capitalizedString }).joinWithSeparator("").lowercaseFirst
+    }
+    
+    var sneakyString: String {
+        let regex = try? NSRegularExpression(pattern: "([a-z])([A-Z])", options: NSRegularExpressionOptions.AllowCommentsAndWhitespace)
+        let value = regex?.stringByReplacingMatchesInString(self, options: .ReportCompletion, range: NSRange(location: 0, length: self.characters.count), withTemplate: "$1_$2")
+        if let result = value where result.isEmpty == false {
+            return result.lowercaseString
+        }
+        return self
+    }
+    
+    var first: String { return String(characters.prefix(1)) }
+    var last: String { return String(characters.suffix(1)) }
+    
+    var uppercaseFirst: String { return first.uppercaseString + String(characters.dropFirst()) }
+    var lowercaseFirst: String { return first.lowercaseString + String(characters.dropFirst()) }
+}
+
+public extension String {
+    var quoted: String {
+        var result = ""
+        if self.hasPrefix("'") == false {
+            result += "'"
+        }
+        result += self
+        if self.hasSuffix("'") == false {
+            result += "'"
+        }
+        return result
     }
 }
