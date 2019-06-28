2c6f632f53381a8d6853b29f4dbe041e09f4c35a
Alex <alexzd89@mail.ru>
2019-06-28 15:22:52 +0400

    Add IS_IPHONE_XS_MAX & HAS_HOME_BUTTON

--- ApplicationSupport/Extensions/Hardware.swift
+++ ApplicationSupport/Extensions/Hardware.swift
@@ -17,6 +17,13 @@ public let IS_IPHONE_5 : Bool = (IS_IPHONE && UIScreen.main.bounds.size.height =
 public let IS_IPHONE_6 : Bool = (IS_IPHONE && UIScreen.main.bounds.size.height == 667.0)
 public let IS_IPHONE_6_PLUS : Bool = (IS_IPHONE && UIScreen.main.bounds.size.height == 736.0)
 public let IS_IPHONE_X : Bool = (IS_IPHONE && UIScreen.main.bounds.size.height == 812.0)
+public let IS_IPHONE_XS_MAX: Bool = (IS_IPHONE && UIScreen.main.bounds.size.height == 896.0)
+public var HAS_HOME_BUTTON: Bool {
+    if #available(iOS 11.0, *), let keyWindow = UIApplication.shared.keyWindow, keyWindow.safeAreaInsets.bottom > 0 {
+        return false
+    }
+    return true
+}
 
 public let IS_IPOD : Bool = UIDevice.current.model.range(of: "iPod") != nil
 
